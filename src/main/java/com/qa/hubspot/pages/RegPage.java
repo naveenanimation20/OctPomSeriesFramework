00a233e28325269a0feef6d7a73a6394bf14e618
Naveen AutomationLabs <naveenanimation20@gmail.com>
2019-12-18 08:39:54 +0530

    Update RegPage.java

--- src/main/java/com/qa/hubspot/pages/RegPage.java
+++ src/main/java/com/qa/hubspot/pages/RegPage.java
@@ -7,6 +7,14 @@ public static void main(){
   System.out.println();
 
 }
+  
+  public void m2(){
+		System.out.println("m2 method....");
+	}
+	
+	public void m3(){
+		System.out.println("m3 method....");
+	}
 
 
 
